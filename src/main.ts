7cf7e55ba77e9e64ffcd1e740d6d93c37504d8ca
Robert Hahn <robert.hahn91@gmx.at>
2023-02-20 13:06:52 +0100

    added more animations

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,25 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+document.addEventListener(
+  'scroll', (event) => {
+    let position = document.documentElement.scrollTop ;
+    console.log(position)
+    if (position > 1400) {
+      document.getElementById('skillsArrow')?.classList.add('arrowAnimation')
+    } 
+
+    if (position > 2450) {
+      document.getElementById('portfolioArrow')?.classList.add('arrowAnimation')
+    }
+  },
+  {passive:true }
+)
+
+
+
+  // function onWindowScroll($event: any) {
+  //   console.log('11')
+  // }
